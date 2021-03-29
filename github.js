f59f7ea5d7533d84ce1e741e5af8a0cb61a140dd
parekh2111 <parekh2111@gmail.com>
2021-03-29 12:25:36 +0100

    first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+	constructor(){
+		this.client_id = '36b49768a156d8c8da33';
+		this.client_secret = 'a43975ae048d14cceb64c427ae1d45e5363bf7de';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
